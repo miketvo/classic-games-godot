4885970e961982b675e39971404e41753a65589a
miketvo <miketvo@outlook.com>
2024-02-15 06:14:17 +0700

    More maintainable code in main_menu.gd

--- p0ng/scripts/scenes/main_menu.gd
+++ p0ng/scripts/scenes/main_menu.gd
@@ -6,7 +6,8 @@ var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 
 func _ready() -> void:
-    var velocity = Vector2.UP * rng.randf_range(160.0, 180.0) 
-    velocity = velocity.rotated(rng.randf_range(0.0, 2.0 * PI))
+    var ball_speed = rng.randf_range(160.0, 180.0)
+    var ball_velocity = Vector2.UP * ball_speed
+    ball_velocity = ball_velocity.rotated(rng.randf_range(0.0, 2.0 * PI))
 
-    ball.set_linear_velocity(velocity)
+    ball.set_linear_velocity(ball_velocity)
