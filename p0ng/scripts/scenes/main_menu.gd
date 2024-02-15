65c93bb19b505e9fccf55651d01ba0163f236014
miketvo <miketvo@outlook.com>
2024-02-16 06:35:06 +0700

    Ready for further implementation

--- p0ng/scripts/scenes/main_menu.gd
+++ p0ng/scripts/scenes/main_menu.gd
@@ -1,13 +1,65 @@
 extends GameScene2D
 
 
-var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-@onready var ball: RigidBody2D = $Ball
+var _rng: RandomNumberGenerator = RandomNumberGenerator.new()
+@onready var _ball: RigidBody2D = $Ball
+@onready var _title_sprite_animation_player: AnimationPlayer = $Title/Sprite2D/AnimationPlayer
 
 
+# ============================================================================ #
+#region Scene setup
 func _ready() -> void:
-    var ball_speed = rng.randf_range(160.0, 180.0)
-    var ball_velocity = Vector2.UP * ball_speed
-    ball_velocity = ball_velocity.rotated(rng.randf_range(0.0, 2.0 * PI))
+    _reset_title()
 
-    ball.set_linear_velocity(ball_velocity)
+    var ball_speed = _rng.randf_range(160.0, 180.0)
+    var ball_velocity = Vector2.UP * ball_speed
+    ball_velocity = ball_velocity.rotated(_rng.randf_range(0.0, 2.0 * PI))
+    _ball.set_linear_velocity(ball_velocity)
+
+    if OS.is_debug_build():
+        print_debug("Scene initialized.")
+#endregion
+# ============================================================================ #
+
+
+# ============================================================================ #
+#region Signal listeners
+
+#region Listens to Title/Walls/*.body_entered(body: Node)
+func _on_top_wall_body_entered(body: Node):
+    if body == _ball:
+        _title_sprite_animation_player.play("collide_top")
+
+
+func _on_left_wall_body_entered(body: Node):
+    if body == _ball:
+        _title_sprite_animation_player.play("collide_left")
+
+
+func _on_bottom_wall_body_entered(body: Node):
+    if body == _ball:
+        _title_sprite_animation_player.play("collide_bottom")
+
+
+func _on_right_wall_body_entered(body: Node):
+    if body == _ball:
+        _title_sprite_animation_player.play("collide_right")
+#endregion
+
+
+#region Listens to Title/Sprite2D/AnimationPlayer.animation_finished(anim_name:StringName)
+func _on_animation_player_animation_finished(anim_name:StringName) -> void:
+    if anim_name != "idle":
+        _reset_title()
+#endregion
+
+#endregion
+# ============================================================================ #
+
+
+# ============================================================================ #
+#region Utils
+func _reset_title():
+    _title_sprite_animation_player.play("idle")
+#endregion
+# ============================================================================ #
--- p0ng/scripts/ui/main_menu_ui.gd
+++ p0ng/scripts/ui/main_menu_ui.gd
@@ -1,5 +1,9 @@
 extends Control
 
 
-func _on_quit_button_pressed() -> void:
+func _on_main_menu_start_button_pressed() -> void:
+	pass
+
+
+func _on_main_menu_quit_button_pressed() -> void:
 	get_tree().quit()
