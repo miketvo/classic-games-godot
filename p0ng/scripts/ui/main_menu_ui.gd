af07285eff83c013722b9d9ff2bde422817768f3
miketvo <miketvo@outlook.com>
2024-02-16 12:03:13 +0700

    Implemented Main Menu tween transitioning styling

--- p0ng/scripts/ui/main_menu_ui.gd
+++ p0ng/scripts/ui/main_menu_ui.gd
@@ -4,10 +4,10 @@ extends Control
 # ============================================================================ #
 #region Godot builtins
 func _ready() -> void:
-	$MainMenuContainer/StartButton.connect("pressed", _on_main_menu_start_button_pressed)
-	$MainMenuContainer/QuitButton.connect("pressed", _on_main_menu_quit_button_pressed)
+    $MainMenuContainer/StartButton.connect("pressed", _on_main_menu_start_button_pressed)
+    $MainMenuContainer/QuitButton.connect("pressed", _on_main_menu_quit_button_pressed)
 
-	$StartMenuContainer/BackButton.connect("pressed", _on_start_menu_back_button_pressed)
+    $StartMenuContainer/BackButton.connect("pressed", _on_start_menu_back_button_pressed)
 #endregion
 # ============================================================================ #
 
@@ -17,19 +17,19 @@ func _ready() -> void:
 
 ## Listens to $MainMenuContainer/QuitButton.pressed()
 func _on_main_menu_start_button_pressed() -> void:
-	_slide_transition($MainMenuContainer, Vector2.LEFT, 0.5)
-	_slide_transition($StartMenuContainer, Vector2.LEFT, 0.5)
+    _slide_transition($MainMenuContainer, Vector2.LEFT, Global.UI_TRANS_DURATION)
+    _slide_transition($StartMenuContainer, Vector2.LEFT, Global.UI_TRANS_DURATION)
 
 
 ## Listens to $MainMenuContainer/QuitButton.pressed()
 func _on_main_menu_quit_button_pressed() -> void:
-	get_tree().quit()
+    get_tree().quit()
 
 
 ## Listens to $StartMenuContainer/BackButton.pressed()
 func _on_start_menu_back_button_pressed() -> void:
-	_slide_transition($MainMenuContainer, Vector2.RIGHT, 0.5)
-	_slide_transition($StartMenuContainer, Vector2.RIGHT, 0.5)
+    _slide_transition($MainMenuContainer, Vector2.RIGHT, Global.UI_TRANS_DURATION)
+    _slide_transition($StartMenuContainer, Vector2.RIGHT, Global.UI_TRANS_DURATION)
 
 #endregion
 # ============================================================================ #
@@ -38,21 +38,25 @@ func _on_start_menu_back_button_pressed() -> void:
 # ============================================================================ #
 #region Utils
 func _slide_transition(
-		ui_element: Control,
-		direction_vector: Vector2,
-		duration: float
+        ui_element: Control,
+        direction_vector: Vector2,
+        duration: float
 ) -> void:
-	if direction_vector not in [ Vector2.UP, Vector2.LEFT, Vector2.DOWN, Vector2.RIGHT ]:
-		assert(false, "MainMenuUI._slide_transition() only accepts unit vector for `direction_vector`")
+    if direction_vector not in Global.UNIT_VECTORS:
+        assert(
+                false,
+                "MainMenuUI._slide_transition() "
+                + "only accepts unit vector for `direction_vector`"
+        )
 
-	var tween = create_tween()
-	tween.tween_property(
-			ui_element, "position",
-			ui_element.position + direction_vector * (
-					get_viewport_rect().size.x / 2 + \
-					ui_element.size.x * ui_element.scale.x / 2
-			),
-			duration
-	)
+    var tween = create_tween()
+    tween.tween_property(
+            ui_element, "position",
+            direction_vector * (
+                    get_viewport_rect().size.x / 2
+                    + ui_element.size.x * ui_element.scale.x / 2
+            ),
+            duration
+    ).as_relative().from_current().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 #endregion
 # ============================================================================ #
--- p0ng/scripts/util/global.gd
+++ p0ng/scripts/util/global.gd
@@ -1,1 +1,11 @@
 extends Node
+
+
+const UNIT_VECTORS: Array = [
+    Vector2.UP,
+    Vector2.LEFT,
+    Vector2.DOWN,
+    Vector2.RIGHT,
+]
+
+const UI_TRANS_DURATION: float = 0.5
