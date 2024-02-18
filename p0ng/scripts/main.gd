8d5cf77fc94ea319aff0c577746dc72a6282d79d
miketvo <miketvo@outlook.com>
2024-02-18 09:33:59 +0700

    Main.set_next_scene() is now private (changed name to _set_next_scene())

--- p0ng/scripts/main.gd
+++ p0ng/scripts/main.gd
@@ -30,7 +30,7 @@ func _on_scene_finished(next_scene_key: SceneKey) -> void:
     _current_scene.queue_free()
     get_tree().root.remove_child(_current_scene)
     _current_scene = null
-    set_next_scene(next_scene_key)
+    _set_next_scene(next_scene_key)
 
 #endregion
 # ============================================================================ #
@@ -38,7 +38,7 @@ func _on_scene_finished(next_scene_key: SceneKey) -> void:
 
 # ============================================================================ #
 #region Utils
-func set_next_scene(next_scene_key: SceneKey) -> void:
+func _set_next_scene(next_scene_key: SceneKey) -> void:
     # Scene change when [param next_scene_key] is provided
     if next_scene_key != SceneKey.NONE:
         _next_scene_key = next_scene_key
