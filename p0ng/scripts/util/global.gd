76d31b1c7df12cd101920fb0bf4f989982594eb4
miketvo <miketvo@outlook.com>
2024-02-18 11:52:04 +0700

    Adjusted game length

--- p0ng/scripts/util/global.gd
+++ p0ng/scripts/util/global.gd
@@ -14,7 +14,7 @@ const PLAYER_SPEED: float = 600.0 ## Unit: px/s
 const BALL_SPEED_INITIAL: float = 450.0 ## Unit: px/s
 const BALL_SPEED_DIFFICULTY_MULTIPLIER: float = 1.15
 const SERVING_ANGULAR_VARIATION: PackedFloat32Array = [-0.392699, 0.392699] ## Unit: radian
-const TARGET_SCORE: int = 5
+const TARGET_SCORE: int = 7
 
 
 func flip_side(side: int) -> int:
