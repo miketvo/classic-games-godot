275a5ac45248ec4a81f7cfcddaf562d436f791fb
miketvo <miketvo@outlook.com>
2024-02-18 10:22:02 +0700

    Added debugging code for Main

--- p0ng/scripts/main.gd
+++ p0ng/scripts/main.gd
@@ -18,6 +18,18 @@ func _process(_delta: float) -> void:
         _current_scene = load(GAME_SCENE[_current_scene_key]).instantiate()
         get_tree().root.add_child(_current_scene)
         _current_scene.connect("scene_finished", _on_scene_finished)
+
+        if OS.is_debug_build():
+            print("========================================")
+            print("LOADED SCENE '%s'" % SceneKey.keys()[_current_scene_key])
+            print("Scene Tree:")
+            self.print_tree()
+            print("Orphan Nodes:")
+            Node.print_orphan_nodes()
+            print("Current Stack:")
+            print_stack()
+            print("========================================")
+            print()
 #endregion
 # ============================================================================ #
 
@@ -29,7 +41,19 @@ func _process(_delta: float) -> void:
 func _on_scene_finished(next_scene_key: SceneKey) -> void:
     _current_scene.queue_free()
     get_tree().root.remove_child(_current_scene)
-    _current_scene = null
+
+    if OS.is_debug_build():
+        print("========================================")
+        print("UNLOADED SCENE '%s'" % SceneKey.keys()[_current_scene_key])
+        print("Scene Tree:")
+        self.print_tree()
+        print("Orphan Nodes:")
+        Node.print_orphan_nodes()
+        print("Current Stack:")
+        print_stack()
+        print("========================================")
+        print()
+
     _set_next_scene(next_scene_key)
 
 #endregion
