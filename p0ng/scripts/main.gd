ee8fd7fb61de2c9d4a6d6c820b30d76aaf786dba
miketvo <miketvo@outlook.com>
2024-02-28 13:45:43 +0700

    Removed tree printing debug code in main.gd, since the new VS Code debugger allows me to inspect the scene tree live

--- p0ng/scripts/main.gd
+++ p0ng/scripts/main.gd
@@ -20,16 +20,6 @@ func _process(_delta: float) -> void:
         move_child(_current_scene, 0)
         _current_scene.connect("scene_finished", _on_scene_finished)
 
-        if OS.is_debug_build():
-            print("========================================")
-            print("LOADED SCENE '%s'" % SceneKey.keys()[_current_scene_key])
-            print("Scene Tree:")
-            print_tree_pretty()
-            print("Current Stack:")
-            print_stack()
-            print("========================================")
-            print()
-
     if Input.is_action_just_pressed("toggle_fullscreen"):
         match get_tree().root.mode:
             Window.MODE_EXCLUSIVE_FULLSCREEN:
@@ -47,16 +37,6 @@ func _process(_delta: float) -> void:
 
 ## Listens to _current_scene.scene_finished(next_scene_key: SceneKey)
 func _on_scene_finished(next_scene_key: SceneKey) -> void:
-    if OS.is_debug_build():
-        print("========================================")
-        print("UNLOADED SCENE '%s'" % SceneKey.keys()[_current_scene_key])
-        print("Scene Tree:")
-        print_tree_pretty()
-        print("Current Stack:")
-        print_stack()
-        print("========================================")
-        print()
-
     _current_scene.queue_free()
     remove_child(_current_scene)
     _current_scene_key = next_scene_key
