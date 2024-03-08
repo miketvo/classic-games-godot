c2cf3413ad2e38fedabe8156ceae82ad7404cf1e
miketvo <miketvo@outlook.com>
2024-03-08 10:30:52 +0700

    Added reset settings menu

--- p0ng/scripts/scenes/settings_menu/settings_menu_ui.gd
+++ p0ng/scripts/scenes/settings_menu/settings_menu_ui.gd
@@ -6,6 +6,7 @@ var input_disabled: bool
 @onready var _main: Container = $Main/Menu/VBoxContainer
 @onready var _graphics: Container = $Graphics
 @onready var _sounds: Container = $Sounds
+@onready var _reset_defaults: Container = $ResetDefaults
 
 
 # ============================================================================ #
@@ -13,12 +14,15 @@ var input_disabled: bool
 func _ready() -> void:
     super()
     input_disabled = false
-    _main.get_node("GraphicsButton").grab_focus()
+    _main.get_node("BackButton").grab_focus()
+    _main.get_node("BackButton").connect("pressed", _on_main_back_button_pressed)
     _main.get_node("GraphicsButton").connect("pressed", _on_main_graphics_button_pressed)
     _main.get_node("SoundsButton").connect("pressed", _on_main_sounds_button_pressed)
-    _main.get_node("BackButton").connect("pressed", _on_main_back_button_pressed)
+    _main.get_node("ResetButton").connect("pressed", _on_main_reset_button_pressed)
     _graphics.get_node("Menu/BackButton").connect("pressed", _on_graphics_menu_back_button_pressed)
     _sounds.get_node("Menu/BackButton").connect("pressed", _on_sounds_menu_back_button_pressed)
+    _reset_defaults.get_node("Menu/CancelButton")\
+            .connect("pressed", _on_reset_defaults_menu_cancel_button_pressed)
 
     for child in get_tree().get_nodes_in_group("ui_container_slider_buttons"):
         assert(child is Button, "ui_container_slider_buttons group must contain only Buttons")
@@ -75,6 +79,15 @@ func _on_main_sounds_button_pressed() -> void:
             .connect("finished", _on_tween_transition_finshed)
 
 
+func _on_main_reset_button_pressed() -> void:
+    input_disabled = true
+    _reset_defaults.get_node("Menu/CancelButton").grab_focus()
+    tween_transition_slide_container($Main, Vector2.UP, UI_TRANSITION_DURATION)\
+            .connect("finished", _on_tween_transition_finshed)
+    tween_transition_slide_container(_reset_defaults, Vector2.UP, UI_TRANSITION_DURATION)\
+            .connect("finished", _on_tween_transition_finshed)
+
+
 func _on_main_back_button_pressed() -> void:
     button_pressed.emit("back_to_main_menu")
 #endregion
@@ -98,6 +111,17 @@ func _on_sounds_menu_back_button_pressed() -> void:
     tween_transition_slide_container(_sounds, Vector2.RIGHT, UI_TRANSITION_DURATION)\
             .connect("finished", _on_tween_transition_finshed)
     tween_transition_slide_container($Main, Vector2.RIGHT, UI_TRANSITION_DURATION)\
+            .connect("finished", _on_tween_transition_finshed)
+#endregion
+
+
+#region Listens to _reset_defaults.get_node("*").
+func _on_reset_defaults_menu_cancel_button_pressed() -> void:
+    input_disabled = true
+    _main.get_node("ResetButton").grab_focus()
+    tween_transition_slide_container(_reset_defaults, Vector2.DOWN, UI_TRANSITION_DURATION)\
+            .connect("finished", _on_tween_transition_finshed)
+    tween_transition_slide_container($Main, Vector2.DOWN, UI_TRANSITION_DURATION)\
             .connect("finished", _on_tween_transition_finshed)
 #endregion
 
