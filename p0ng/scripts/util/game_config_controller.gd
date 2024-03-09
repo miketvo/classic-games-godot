75c1cbfaac8758f2ede995253b7d711b2d1b7980
miketvo <miketvo@outlook.com>
2024-03-09 10:17:29 +0700

    Updated GameConfigController backend to have proper linear volume percentage to logarithmic decibel scale conversion

--- p0ng/scripts/util/game_config_controller.gd
+++ p0ng/scripts/util/game_config_controller.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 
 const MIN_AUDIO_BUS_VOLUME_DB: float = -80.0
+const MAX_AUDIO_BUS_VOLUME_DB: float = 0.0
 
 @export var post_processing_node: WorldEnvironment
 @export var crt_effect_node: ColorRect
@@ -90,14 +91,15 @@ func _update_graphics_crt_effect_mode() -> void:
 func _update_sounds_master_bus() -> void:
     for bus_index in range(AudioServer.bus_count):
         if AudioServer.get_bus_name(bus_index) == "Master":
-            var volume_range_db: float = abs(
-                    _master_bus_max_volume_db - MIN_AUDIO_BUS_VOLUME_DB
+            var volume_percentage: float = (
+                    GameConfig.config.sounds.master_volume
+                    / GameConfig.VOLUME_SLIDER_MAX_VALUE
             )
             var volume_db: float = (
-                    MIN_AUDIO_BUS_VOLUME_DB
-                    + (volume_range_db / GameConfig.VOLUME_SLIDER_MAX_VALUE)
-                    * GameConfig.config.sounds.master_volume
+                    20.0 * log(volume_percentage) / log(10.0)
+                    + _master_bus_max_volume_db
             )
+            volume_db = clampf(volume_db, MIN_AUDIO_BUS_VOLUME_DB, MAX_AUDIO_BUS_VOLUME_DB)
 
             AudioServer.set_bus_volume_db(bus_index, volume_db)
             AudioServer.set_bus_mute(bus_index, GameConfig.config.sounds.master_muted)
@@ -107,14 +109,15 @@ func _update_sounds_master_bus() -> void:
 func _update_sounds_ui_bus() -> void:
     for bus_index in range(AudioServer.bus_count):
         if AudioServer.get_bus_name(bus_index) == "UI":
-            var volume_range_db: float = abs(
-                    _ui_bus_max_volume_db - MIN_AUDIO_BUS_VOLUME_DB
+            var volume_percentage: float = (
+                    GameConfig.config.sounds.ui_volume
+                    / GameConfig.VOLUME_SLIDER_MAX_VALUE
             )
             var volume_db: float = (
-                    MIN_AUDIO_BUS_VOLUME_DB
-                    + (volume_range_db / GameConfig.VOLUME_SLIDER_MAX_VALUE)
-                    * GameConfig.config.sounds.ui_volume
+                    20.0 * log(volume_percentage) / log(10.0)
+                    + _ui_bus_max_volume_db
             )
+            volume_db = clampf(volume_db, MIN_AUDIO_BUS_VOLUME_DB, MAX_AUDIO_BUS_VOLUME_DB)
 
             AudioServer.set_bus_volume_db(bus_index, volume_db)
             AudioServer.set_bus_mute(bus_index, GameConfig.config.sounds.ui_muted)
@@ -124,14 +127,15 @@ func _update_sounds_ui_bus() -> void:
 func _update_sounds_gameplay_bus() -> void:
     for bus_index in range(AudioServer.bus_count):
         if AudioServer.get_bus_name(bus_index) == "Gameplay":
-            var volume_range_db: float = abs(
-                    _gameplay_bus_max_volume_db - MIN_AUDIO_BUS_VOLUME_DB
+            var volume_percentage: float = (
+                    GameConfig.config.sounds.gameplay_volume
+                    / GameConfig.VOLUME_SLIDER_MAX_VALUE
             )
             var volume_db: float = (
-                    MIN_AUDIO_BUS_VOLUME_DB
-                    + (volume_range_db / GameConfig.VOLUME_SLIDER_MAX_VALUE)
-                    * GameConfig.config.sounds.gameplay_volume
+                    20.0 * log(volume_percentage) / log(10.0)
+                    + _gameplay_bus_max_volume_db
             )
+            volume_db = clampf(volume_db, MIN_AUDIO_BUS_VOLUME_DB, MAX_AUDIO_BUS_VOLUME_DB)
 
             AudioServer.set_bus_volume_db(bus_index, volume_db)
             AudioServer.set_bus_mute(bus_index, GameConfig.config.sounds.gameplay_muted)
