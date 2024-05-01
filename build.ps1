4ec44708ca3d9b20c0110447b6e996707c14e710
miketvo <miketvo@outlook.com>
2024-05-01 15:54:58 +0700

    Workaround for issue with async godot cli command breaking archiving function

--- build.ps1
+++ build.ps1
@@ -81,7 +81,7 @@ foreach ($project in $godotProjects) {
 
         Write-Output "Exporting $projectName ($exportPreset) ..."
         Set-Location -Path $project
-        godot --headless --verbose $godotExportFlag $exportPreset
+        godot --headless --verbose $godotExportFlag $exportPreset | Out-Null
         Set-Location -Path $PSScriptRoot
 
         $zipFileName = "$projectName-$exportPreset.zip"
