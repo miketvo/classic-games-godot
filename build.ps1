a94785a8c2ceda58fda34cf06478a71f0e445e6f
miketvo <miketvo@outlook.com>
2024-08-07 16:54:07 +0700

    build.ps1: Removed wait code

--- build.ps1
+++ build.ps1
@@ -81,7 +81,6 @@ foreach ($project in $godotProjects) {
 
         Write-Host "Exporting $projectName ($exportPreset) ..." -ForegroundColor Yellow
         godot --headless --path $project $godotExportFlag $exportPreset | Out-Default
-        Start-Sleep -Seconds 10.0
 
         $zipFileName = "$projectName-$exportPreset.zip"
         $zipFilePath = Join-Path -Path $releaseFolderPath -ChildPath $zipFileName
